a9115f205ac4b8d303429bd9ae05ef1ef8abd6fa
yukult <gula421@gmail.com>
2021-02-08 16:26:29 +0100

    before security folder

---
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -0,0 +1,39 @@
+package com.example.demo.model.requests;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CreateUserRequest {
+
+	@JsonProperty
+	private String username;
+
+	@JsonProperty
+	private String password;
+
+	@JsonProperty
+	private String passwordConfirm;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
